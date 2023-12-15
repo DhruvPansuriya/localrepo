38d1435c393ea7e607687e0b6fa67edf58ee5179
DhruvPansuriya <dhruvpansuriya05@gmail.com>
2023-12-15 14:47:30 +0530

    Add initial file

--- GitInfoText.cpp
+++ GitInfoText.cpp
@@ -2,17 +2,11 @@
 Clone: cloning a repository on our local machine
 
    type this-> git clone <-some link-> to add the github file.
-    cd means change directory
+    // cd means change directory
    
    type-> git status  to find status.
    type-> git add filename  to make file staged.
    type-> git commit -m "message"  to make file committed.
-
-
-
-
-
-
-
-*/
-
+   type-> git push origin main to upload our localrepo to github.
+   type-> git init to make this folder a git repo.
+ 
